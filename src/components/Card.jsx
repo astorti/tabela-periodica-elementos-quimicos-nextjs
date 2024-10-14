7e5a206582886335043a0b6302658057002e4b7f
André Storti Andrade <a.storti@hotmail.com>
2024-10-14 18:27:23 -0300

    Estilização dinâmica dos cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,15 +5,92 @@ const Card = ({
   pontoEbulicao, periodo, familia, grupo, distribuicaoEletronica,
   estadoNatural, uso1, uso2, uso3
 }) => {
+
+  let mainCard = "flex justify-around w-4/5 border rounded border-white m-auto mt-28 mb-40";
+  let symbolCard = "flex flex-col w-32 h-40 border justify-around rounded border-white-400 p-2 mb-5"
+  let symbol = "text-7xl text-center text-white-200"
+  let line1 = "text-xl font-bold mt-10 border-b border-b-white-300"
+  let line2 = "mt-4 mb-2 border-b border-b-white-300"
+
+  let colorCards = function () {
+    if (grupo === "Hidrogênio") {
+      mainCard = "flex justify-around w-4/5 border rounded border-stone-400 bg-stone-600 m-auto mt-28 mb-40"
+      symbolCard = "flex flex-col w-32 h-40 border justify-around rounded border-stone-400 p-2 mb-5"
+      symbol = "text-7xl text-center text-stone-200"
+      line1 = "text-xl font-bold mt-10 border-b border-b-stone-300"
+      line2 = "mt-4 mb-2 border-b border-b-stone-300"
+    } else if (grupo === "metais de transição") {
+        mainCard = "flex justify-around w-4/5 border rounded border-teal-600 bg-teal-600/40 m-auto mt-28 mb-40"
+        symbolCard = "flex flex-col w-32 h-40 border justify-around rounded border-teal-400 p-2 mb-5"
+        symbol = "text-7xl text-center text-teal-200"
+        line1 = "text-xl font-bold mt-10 border-b border-b-teal-300"
+        line2 = "mt-4 mb-2 border-b border-b-teal-300"
+    } else if (grupo === "metais alcalinos") {
+        mainCard = "flex justify-around w-4/5 border rounded border-yellow-600 bg-yellow-600/80 m-auto mt-28 mb-40"
+        symbolCard = "flex flex-col w-32 h-40 border justify-around rounded border-yellow-400 p-2 mb-5"
+        symbol = "text-7xl text-center text-yellow-200"
+        line1 = "text-xl font-bold mt-10 border-b border-b-yellow-300"
+        line2 = "mt-4 mb-2 border-b border-b-yellow-300"
+    } else if (grupo === "metais alcalino-terrosos") {
+        mainCard = "flex justify-around w-4/5 border rounded border-orange-600 bg-orange-600/80 m-auto mt-28 mb-40"
+        symbolCard = "flex flex-col w-32 h-40 border justify-around rounded border-orange-400 p-2 mb-5"
+        symbol = "text-7xl text-center text-orange-200"
+        line1 = "text-xl font-bold mt-10 border-b border-b-orange-300"
+        line2 = "mt-4 mb-2 border-b border-b-orange-300"
+    } else if (grupo === "gases nobres") {
+        mainCard = "flex justify-around w-4/5 border rounded border-purple-600 bg-purple-600/80 m-auto mt-28 mb-40"
+        symbolCard = "flex flex-col w-32 h-40 border justify-around rounded border-purple-400 p-2 mb-5"
+        symbol = "text-7xl text-center text-purple-200"
+        line1 = "text-xl font-bold mt-10 border-b border-b-purple-300"
+        line2 = "mt-4 mb-2 border-b border-b-purple-300"
+    } else if (grupo === "halogênios") {
+        mainCard = "flex justify-around w-4/5 border rounded border-fuchsia-400 bg-fuchsia-400/80 m-auto mt-28 mb-40"
+        symbolCard = "flex flex-col w-32 h-40 border justify-around rounded border-fuchsia-400 p-2 mb-5"
+        symbol = "text-7xl text-center text-fuchsia-200"
+        line1 = "text-xl font-bold mt-10 border-b border-b-fuchsia-300"
+        line2 = "mt-4 mb-2 border-b border-b-fuchsia-300"
+    } else if (grupo === "metais pós-transição") {
+        mainCard = "flex justify-around w-4/5 border rounded border-red-600 bg-red-600/80 m-auto mt-28 mb-40"
+        symbolCard = "flex flex-col w-32 h-40 border justify-around rounded border-red-400 p-2 mb-5"
+        symbol = "text-7xl text-center text-red-200"
+        line1 = "text-xl font-bold mt-10 border-b border-b-red-300"
+        line2 = "mt-4 mb-2 border-b border-b-red-300"
+    } else if (grupo === "semimetais") {
+        mainCard = "flex justify-around w-4/5 border rounded border-blue-600 bg-blue-600/80 m-auto mt-28 mb-40"
+        symbolCard = "flex flex-col w-32 h-40 border justify-around rounded border-blue-400 p-2 mb-5"
+        symbol = "text-7xl text-center text-blue-200"
+        line1 = "text-xl font-bold mt-10 border-b border-b-blue-300"
+        line2 = "mt-4 mb-2 border-b border-b-blue-300"
+    } else if (grupo === "ametais") {
+        mainCard = "flex justify-around w-4/5 border rounded border-emerald-600 bg-emerald-600/80 m-auto mt-28 mb-40"
+        symbolCard = "flex flex-col w-32 h-40 border justify-around rounded border-emerald-400 p-2 mb-5"
+        symbol = "text-7xl text-center text-emerald-200"
+        line1 = "text-xl font-bold mt-10 border-b border-b-emerald-300"
+        line2 = "mt-4 mb-2 border-b border-b-emerald-300"
+    } else if (grupo === "lantanídeos") {
+        mainCard = "flex justify-around w-4/5 border rounded border-slate-600 bg-slate-600/80 m-auto mt-28 mb-40"
+        symbolCard = "flex flex-col w-32 h-40 border justify-around rounded border-slate-400 p-2 mb-5"
+        symbol = "text-7xl text-center text-slate-200"
+        line1 = "text-xl font-bold mt-10 border-b border-b-slate-300"
+        line2 = "mt-4 mb-2 border-b border-b-slate-300"
+    } else if (grupo === "actinídeos") {
+        mainCard = "flex justify-around w-4/5 border rounded border-sky-300 bg-sky-400/80 m-auto mt-28 mb-40"
+        symbolCard = "flex flex-col w-32 h-40 border justify-around rounded border-sky-400 p-2 mb-5"
+        symbol = "text-7xl text-center text-sky-200"
+        line1 = "text-xl font-bold mt-10 border-b border-b-sky-300"
+        line2 = "mt-4 mb-2 border-b border-b-sky-300"
+    }
+  }
+
+  colorCards()
+
   return (
-    <div className="flex justify-around w-4/5 border rounded 
-                  border-cyan-400 bg-cyan-200/40 m-auto mt-28 mb-40">
+    <div className={mainCard}>
       <section className="flex flex-col justify-center">
-        <div className="flex flex-col w-32 h-40 border justify-around
-                  rounded border-cyan-400 p-2 mb-5"
+        <div className={symbolCard}
         >
           <p>{numeroAtomico}</p>
-          <p className="text-7xl text-center text-cyan-200">{simbolo}</p>
+          <p className={symbol}>{simbolo}</p>
           <p className="text-center">{nome}</p>
         </div>
 
@@ -30,7 +107,7 @@ const Card = ({
       </section>
 
       <div>
-        <p className="text-xl font-bold mt-10 border-b border-b-cyan-300">Propriedades</p>
+        <p className={line1}>Propriedades</p>
         <div className="flex mt-5 mb-10">
           <section className="mr-10">
             <div className="flex justify-between">
@@ -101,7 +178,7 @@ const Card = ({
             </div>
 
             <section>
-              <p className="mt-4 mb-2 border-b border-b-cyan-300">Principais usos</p>
+              <p className={line2}>Principais usos</p>
               <section>
                 <div className="flex">
                   <p className="w-5 font-bold">1:</p>
